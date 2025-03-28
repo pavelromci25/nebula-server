44ff363cae0ef23f91862b0992abf6cd36d53439
pavelromci25 <pavel.romci25@gmail.com>
2025-03-28 23:07:31 +0200

    Updated server with coins support

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'https://pavelromci25.github.io', // Разрешаем только твой фронтенд
+  origin: 'https://pavelromci25.github.io',
 }));
-
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://pavelromci25:lpUHGXAkgIaAbnnT@nebula.th0fboc.mongodb.net/nebula?retryWrites=true&w=majority')
@@ -22,6 +21,12 @@ const GameSchema = new mongoose.Schema({
 });
 const Game = mongoose.model('Game', GameSchema);
 
+const CoinSchema = new mongoose.Schema({
+  userId: String,
+  coins: Number,
+});
+const Coin = mongoose.model('Coin', CoinSchema);
+
 app.get('/api/games', async (req, res) => {
   const games = await Game.find();
   res.json(games);
@@ -36,6 +41,17 @@ async function seedDatabase() {
   console.log('Тестовые данные добавлены');
 }
 seedDatabase();
+
+app.post('/api/coins', async (req, res) => {
+  const { userId, coins } = req.body;
+  await Coin.updateOne({ userId }, { $inc: { coins } }, { upsert: true });
+  res.sendStatus(200);
+});
+
+app.get('/api/coins/:userId', async (req, res) => {
+  const coins = await Coin.findOne({ userId: req.params.userId });
+  res.json({ coins: coins?.coins || 0 });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Сервер запущен на порту ${port}`));
